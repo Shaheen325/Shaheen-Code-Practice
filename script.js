33e5919b5c269c96737e41e3b24f99d004f66546
Shaheen Awan <shaheenawan70@gmail.com>
2026-07-25 21:11:39 +0500

    constant and let of js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("welcome to my first code file");
+alert("welcome to my first code file......");
+let name = "shaheen"
+let age = 28
+let learning = "javascript"
+console.log(name);
+console.log(age);
+console.log(learning);
+
+let city ="Lahore"
+console.log(city);
+city = "Karachi"
+console.log(city);
+
+const country = "Pakistan"
+console.log(country);
+country = "India"
+console
