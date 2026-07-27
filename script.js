bb979c914a5b71c92b59e5eebee0c4d9db923049
Shaheen Awan <shaheenawan70@gmail.com>
2026-07-27 21:30:15 +0500

    day 5 tak

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let name = "shaheen"
 let age = 28
 let learning = "javascript"
 console.log(name);
+console.log(typeof name);
 console.log(age);
+console.log(typeof age);
 console.log(learning);
-
+console.log(typeof learning);
 let city ="Lahore"
 console.log(city);
 city = "Karachi"
@@ -13,5 +15,43 @@ console.log(city);
 
 const country = "Pakistan"
 console.log(country);
-country = "India"
-console
+
+let email;
+console.log(email);
+console.log(typeof email);
+
+let user = {
+    name: "shaheen",
+    age: 28,
+    city: "Lahore",
+    email: "shaheen@gamil.com"
+}
+console.log(user);
+console.log(typeof user);
+
+let color = ["red", "green", "blue", "yellow"]
+console.log(color);
+console.log(typeof color);
+
+// object ka ander array
+let product = {
+    title: "wood Floor",
+    price: 400,
+    images: ["1.jpg", "2.jpg", "3.jpg"]
+}
+console.log(product.images[2]);
+
+// array k ander object
+
+let Product = [
+    {
+        title:"chair",
+        Price: 2000
+    },
+
+    {
+        title: "table",
+        Price: 4000
+    }
+]
+console.log(Product[0].title)
